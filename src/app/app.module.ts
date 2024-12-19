bfc58c5a098ecbc6cf5072ee046f8d8ee12e9469
Jose-Fernan-22 <choquejose295@gmail.com>
2024-12-18 22:44:48 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarProductosComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // Es necesario para que     ngx-toastr funcione
+    ToastrModule.forRoot()   // Configuración básica de ngx-toastr
   ],
   providers: [],
   bootstrap: [AppComponent]
